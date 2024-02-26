8650585a91d25f9bc35c0567a29e02fea43e91a9
bayazidsustami <bayazidsustamy@gmail.com>
2024-02-26 20:30:14 +0800

    initial commit

---
+++ src/main/java/com/example/asteroids/nasa/NasaAsteroidsApplication.java
@@ -0,0 +1,13 @@
+package com.example.asteroids.nasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaAsteroidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaAsteroidsApplication.class, args);
+	}
+
+}
