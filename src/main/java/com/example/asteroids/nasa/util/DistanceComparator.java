bd827a14a5f19b8068e044470693a9ec9d0817c0
bayazidsustami <bayazidsustamy@gmail.com>
2024-02-27 20:12:27 +0800

    fix : wrong comparable,  should be sort from smallest distance

--- src/main/java/com/example/asteroids/nasa/util/DistanceComparator.java
+++ src/main/java/com/example/asteroids/nasa/util/DistanceComparator.java
@@ -8,7 +8,7 @@ public class DistanceComparator implements Comparator<AsteroidResponse> {
 
     @Override
     public int compare(AsteroidResponse o1, AsteroidResponse o2) {
-        return o2.getCloseApproachData().getDistanceToEarth().compareTo(
-                o1.getCloseApproachData().getDistanceToEarth());
+        return o1.getCloseApproachData().getDistanceToEarth().compareTo(
+                o2.getCloseApproachData().getDistanceToEarth());
     }
 }
