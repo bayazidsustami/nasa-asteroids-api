137da056649081f89f87c0e81235f984e043d6ca
bayazidsustami <bayazidsustamy@gmail.com>
2024-02-28 10:08:42 +0800

    feat : add global exception for runtime exception

--- src/main/java/com/example/asteroids/nasa/controller/ErrorController.java
+++ src/main/java/com/example/asteroids/nasa/controller/ErrorController.java
@@ -1,6 +1,7 @@
 package com.example.asteroids.nasa.controller;
 
 import com.example.asteroids.nasa.models.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,6 +18,18 @@ public class ErrorController {
                 .status(exception.getStatusCode())
                 .body(ErrorResponse.builder()
                         .message(exception.getReason())
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> runtimeException(
+            RuntimeException exception
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ErrorResponse.builder()
+                        .message(exception.getMessage())
                         .build()
                 );
     }
